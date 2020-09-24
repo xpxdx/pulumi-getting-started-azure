739665af747bc63fc6aaf9a1df4aed23f54ff68f
Jim Counts <jamesrcounts@outlook.com>
2020-09-24 14:40:08 +0000

    Update to dotnet base image

--- test-build.sh
+++ test-build.sh
@@ -3,6 +3,7 @@ set -euo pipefail
 
 docker pull mcr.microsoft.com/vscode/devcontainers/base:buster
 docker build \
+    --build-arg VARIANT=3.1 \
     --build-arg INSTALL_AZURE_CLI=true \
     --build-arg INSTALL_DOCKER=true \
     --build-arg INSTALL_NODE=false \
