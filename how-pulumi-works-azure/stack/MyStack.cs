239362cb781a253c5be13a0d972293964751294b
Jim Counts <jamesrcounts@outlook.com>
2020-09-25 14:36:43 +0000

    Experimenting with storage accounts

--- how-pulumi-works-azure/stack/MyStack.cs
+++ how-pulumi-works-azure/stack/MyStack.cs
@@ -1,6 +1,7 @@
 using Pulumi;
 using Pulumi.Azure.Core;
 using Pulumi.Azure.Storage;
+using Pulumi.Azure.Storage.Inputs;
 
 class MyStack : Stack
 {
@@ -9,22 +10,34 @@ class MyStack : Stack
         // Create an Azure Resource Group
         var resourceGroup = new ResourceGroup("resourceGroup");
 
-        // Create an Azure Storage Account
-        var storageAccount = new Account("storage", new AccountArgs
+
+        // Create Azure Storage Accounts
+        AccountArgs storageConfig = new AccountArgs
         {
             ResourceGroupName = resourceGroup.Name,
-            AccountReplicationType = "LRS",
+            AccountReplicationType = "RAGRS",
             AccountTier = "Standard",
-            Tags = 
+            MinTlsVersion = "TLS1_2",
+            BlobProperties = new AccountBlobPropertiesArgs
             {
-                { "Environment", "Dev" }
+                DeleteRetentionPolicy = new AccountBlobPropertiesDeleteRetentionPolicyArgs { Days = 30 }
+            },
+            Tags = new InputMap<string> {
+                {"stack", "Dev"}
             }
-        });
+        };
 
-        // Export the connection string for the storage account
-        this.ConnectionString = storageAccount.PrimaryConnectionString;
+        var mediaAccount = new Account("media", storageConfig);
+        var contentAccount = new Account("content", storageConfig);
+
+        // Export the connection strings for the storage accounts
+        this.MediaConnectionString = mediaAccount.PrimaryConnectionString;
+        this.ContentConnectionString = mediaAccount.PrimaryConnectionString;
     }
 
     [Output]
-    public Output<string> ConnectionString { get; set; }
+    public Output<string> MediaConnectionString { get; set; }
+
+    [Output]
+    public Output<string> ContentConnectionString { get; set; }
 }
